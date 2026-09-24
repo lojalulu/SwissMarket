ea50c7ee5adb066abadcf3834b818a22a46848db
lojalulu <sac.lojalulu@gmail.com>
2026-09-25 01:53:48 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// Caminho no repositório: next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+};
+
+module.exports = nextConfig;
